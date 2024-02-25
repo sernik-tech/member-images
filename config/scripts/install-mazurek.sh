2e2d76b0de870404e1ae331c32f48548b0346961
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-02-25 00:47:10 -0600

    chore(mazurek): reenable swaylock-effects

--- config/scripts/install-mazurek.sh
+++ config/scripts/install-mazurek.sh
@@ -10,15 +10,14 @@ wget https://copr.fedorainfracloud.org/coprs/swayfx/swayfx/repo/fedora-${OS_VERS
 # swaync/SwayNotificationCenter
 wget https://copr.fedorainfracloud.org/coprs/erikreider/SwayNotificationCenter/repo/fedora-${OS_VERSION}/erikreider-SwayNotificationCenter-fedora-${OS_VERSION}.repo?arch=x86_64 -O /etc/yum.repos.d/_copr_erikreider-SwayNotificationCenter.repo
 # swaylock-effects
-#wget https://copr.fedorainfracloud.org/coprs/vrumger/swaylock-effects/repo/fedora-${OS_VERSION}/swayfx-swayfx-fedora-${OS_VERSION}.repo?arch=x86_64 -O /etc/yum.repos.d/_copr_vrumger-swaylock-effects.repo
+wget https://copr.fedorainfracloud.org/coprs/vrumger/swaylock-effects/repo/fedora-${OS_VERSION}/swayfx-swayfx-fedora-${OS_VERSION}.repo?arch=x86_64 -O /etc/yum.repos.d/_copr_vrumger-swaylock-effects.repo
 
 # install packages
 echo "Installing packages"
 
-rpm-ostree install swayfx SwayNotificationCenter
-#swaylock-effects
+rpm-ostree install swayfx SwayNotificationCenter swaylock-effects
 
 # disable repos afterwards
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_swayfx-swayfx.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_erikreider-SwayNotificationCenter.repo
-#sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_vrumger-swaylock-effects.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_vrumger-swaylock-effects.repo
