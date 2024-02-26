dd019e60dcea0cf5dc2e985794eb5f677dcc53a8
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-02-26 13:53:24 -0600

    chore(wuzetka): remove throttled

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -13,16 +13,14 @@ wget "https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fe
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite.repo"
 # multilib
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-multilib-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo"
-# throttled
-wget "https://copr.fedorainfracloud.org/coprs/abn/throttled/repo/fedora-$(rpm -E %fedora)/abn-throttled-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_abn-throttled.repo"
 
 # installing the packages
 
 # install power related utilities, ensuring no conflicts exist first
 if rpm -qa | grep power-profiles-daemon ; then
-    rpm-ostree override remove power-profiles-daemon --install=tlp --install=tlp-rdw --install=throttled
+    rpm-ostree override remove power-profiles-daemon --install=tlp --install=tlp-rdw
 else
-    rpm-ostree install tlp tlp-rdw throttled
+    rpm-ostree install tlp tlp-rdw
 fi
 
 # console/terminal related packages
@@ -45,11 +43,9 @@ rpm-ostree install crontabs gamemode gamescope joystickwake mangohud xwiimote-ng
 
 # enable any systemd services
 systemctl enable tlp
-systemctl enable throttled
 
 # disabling the respositories for the booted system and later user packages (since they'll be dealt with in image creation)
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_atim-starship.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_wezfurlong-wezterm-nightly.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_abn-throttled.repo
