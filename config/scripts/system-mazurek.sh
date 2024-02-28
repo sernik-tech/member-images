addfb7134e96cdd5052efed0183d692624d74782
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-02-27 20:49:10 -0600

    chore(mazurek): force remove sway:

--- config/scripts/system-mazurek.sh
+++ config/scripts/system-mazurek.sh
@@ -11,22 +11,16 @@ wget "https://copr.fedorainfracloud.org/coprs/swayfx/swayfx/repo/fedora-$(rpm -E
 wget "https://copr.fedorainfracloud.org/coprs/sneexy/swayland/repo/fedora-$(rpm -E %fedora)/sneexy-swayland-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_sneexy-swayland.repo"
 # bazzite repositories
 
-# installing the packages
+# managing the packages
 
-# install power related utilities, ensuring no conflicts exist first
-if rpm -qa | grep power-profiles-daemon ; then
-    rpm-ostree override remove power-profiles-daemon --install=tlp --install=tlp-rdw
-else
-    rpm-ostree install tlp tlp-rdw
-fi
+# removing base packages
+rpm-ostree override remove sway
 
-# console/terminal related packages
+# swayfx and friends
 rpm-ostree install swayfx sway-audio-idle-inhibit
 
 # disabling the respositories for the booted system and later user packages (since they'll be dealt with in image creation)
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_atim-starship.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_wezfurlong-wezterm-nightly.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_swayfx-swayfx.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_sneexy-swayland.repo
 
 # refer to system-wezetka to install the personal system packages
