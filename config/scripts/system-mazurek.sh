073286522946dd19bd0211122d1ec44f3f1ac08a
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-02 11:22:53 -0600

    chore(mazurek): remove unused copr, add sddm, remove containerfiles

--- config/scripts/system-mazurek.sh
+++ config/scripts/system-mazurek.sh
@@ -9,8 +9,6 @@ set -euo pipefail
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite.repo"
 # multilib
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-multilib-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo"
-# swaylock
-#wget "https://copr.fedorainfracloud.org/coprs/vrumger/swaylock-effects/repo/fedora-$(rpm -E %fedora)/vrumger-swaylock-effects-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_vrumger-swaylock-effects.repo"
 
 # bazzite packages
 rpm-ostree install joystickwake xwiimote-ng
@@ -27,8 +25,6 @@ fi
 
 # disabling the respositories for the booted system (since they'll be dealt with in image creation)
 # sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/$(rpm -E %fedora).repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/swayfx-swayfx-fedora-$(rpm -E %fedora).repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/sneexy-swayland-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/erikreider-SwayNotificationCenter-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/solopasha-hyprland-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tofik-nwg-shell-fedora-$(rpm -E %fedora).repo
@@ -36,4 +32,3 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/home:manuelschneid3r.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezfurlong-wezterm-nightly-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
-#sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_vrumger-swaylock-effects.repo
