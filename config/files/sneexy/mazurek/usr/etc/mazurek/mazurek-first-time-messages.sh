84de6a44b780712b09c61a4b7fafd4a2819e9dd1
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-09 18:54:53 -0600

    chore(mazurek): add power-profiles-daemon + aaa

---
+++ config/files/sneexy/mazurek/usr/etc/mazurek/mazurek-first-time-messages.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+img=$(find "/etc/mazurek/assets" -type f -print0 | shuf -z -n 1)
+
+zenity --info --width=400 --height=280 --icon="face-smile-big" --title="Welcome to Mazurek" --text="These windows will give you the basic information on how to navigate around your system.\n\nPlease do remember that this is a <i>completely personalized</i> for the images' creator, and that most things here are also things that either aren't compatible with your system/setup or things you just do not want.\n\nYou can find the <a href='https://git.gay/sneexy/el-dotfiles'>same exact dotfiles used here</a> over in its respective repository."
+
+zenity --info --width=400 --height=480 --icon="input-keyboard" --title="Hyprland Keybinds" --text="<b>boobies</b> • boobies"
+
+zenity --info --width=400 --height=480 --icon="input-keyboard" --title="Application Keybinds" --text="</b>\n<b>⊞ Super + Enter</b> • Opens a terminal\n<b>⊞ Super + Control + F</b> • Open Dolphin file manager\n<b>⊞ Super + Control + B</b> • Opens Firefox web browser"
+
+zenity --info --width=400 --height=250 --icon="$img" --ok-label="OK • mrrp meow :3 • rraawr x3" --title="That's all" --text="To see these messages again, run the script located at <i>/etc/mazurek/mazurek-first-time-messages.sh</i>.\n\n<a href='https://github.com/sernik-tech/member-images'>Sernik member-images repository</a>\n\nBy clicking the button below, you <b>100% fully agree</b> to trans rights, becoming a boykisser, owning at least 73 cats, snuggling your local dragons and foxes, destroying capitalism and eating your enemies.\n\n<small>Icon credits to <a href='https://www.ahnik.art/'>Ahnik</a></small>"
---
+++ config/files/sneexy/mazurek/usr/etc/mazurek/mazurek-user-setup.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# This script will try to automate copying configuration files to the right place
+# for new users logging in.
+# It's powered by a custom user systemd unit that runs this script then disables
+# itself when finished. First touches a file before running to prevent accidentally
+# overridding any configuration or files.
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+# Confirm that the file doesn't exist and don't run if it does.
+if [ -f $HOME/.local/share/.mazurek-post ]; then
+    echo "Script has already been ran before. Exiting."
+    if "systemctl --user status mazurek-user-setup.service | grep '; enabled;'"; then
+        systemctl --user disable mazurek-user-setup.service
+    fi
+    return 1
+    exit
+fi
+
+# All of our custom configurations and files are located under /usr/etc/mazurek if you
+# want to look at them.
+
+# We'll make sure the directories exist first.
+mkdir -p $HOME/.config/hypr
+mkdir -p $HOME/.config/waybar
+mkdir -p $HOME/.config/ags
+mkdir -p $HOME/.config/swaync
+mkdir -p $HOME/.local/bin
+
+# We'll copy the files to the local user's directories, if they exist.
+
+cp -rf /etc/mazurek/hypr/* $HOME/.config/hypr
+cp -rf /etc/mazurek/waybar/* $HOME/.config/waybar
+cp -rf /etc/mazurek/ags/* $HOME/.config/ags
+cp -rf /etc/mazurek/swaync/* $HOME/.config/swaync
+cp /etc/mazurek/bin/* $HOME/.local/bin
+
+# Ensure the files are executable.
+chmod +x $HOME/.local/bin/focus-mode
+chmod +x $HOME/.local/bin/restart-portal-wlr
+chmod +x $HOME/.local/bin/swww-random
+
+# Run the extra stuff afterwards.
+hyprctl dispatch exec waybar
+hyprctl dispatch exec swaync
+
+# Reload hyprland config.
+hyprctl reload
+
+# Disable service.
+systemctl --user disable mazurek-user-setup.service
+
+# Touch file to prevent this script from running twice.
+touch $HOME/.local/share/.mazurek-post
+
+# Launch first time messages.
+/bin/bash -c "$(/etc/mazurek/mazurek-first-time-messages.sh)"
