00968899755fb442e5385e38e9866d0a2dc618b4
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-18 17:42:25 -0500

    chore(mazurek): re-add kitty

--- config/scripts/system-mazurek.sh
+++ config/scripts/system-mazurek.sh
@@ -14,7 +14,7 @@ wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fe
 rpm-ostree install joystickwake xwiimote-ng
 
 # remove kitty terminal (needs to be done here because its after hyprland is installed)
-rpm-ostree override remove kitty
+# rpm-ostree override remove kitty
 
 #
 # create all required folders for the following setup
