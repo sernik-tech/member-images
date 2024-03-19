adeb67ab85cb23c771a9149ef9a9a66108a9de8b
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-19 07:51:16 -0500

    chore(wuzetka): compiling doesn't work

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -33,16 +33,16 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multil
 
 # NOTE: You need to change this for plasma 6
 # Install development packages & ensure correct paths are exported
-rpm-ostree install binutils cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
-export PATH=$PATH:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
+#rpm-ostree install binutils cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
+#export PATH=$PATH:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
 
 # Clone repo and compile & install effects
-git clone https://github.com/matinlotfali/KDE-Rounded-Corners /tmp/rounded-corners
-mkdir -p /tmp/rounded-corners/build
-cd /tmp/rounded-corners/build && cmake .. --install-prefix /usr
-cd /tmp/rounded-corners/build && make
-cd /tmp/rounded-corners/build && make install
+#git clone https://github.com/matinlotfali/KDE-Rounded-Corners /tmp/rounded-corners
+#mkdir -p /tmp/rounded-corners/build
+#cd /tmp/rounded-corners/build && cmake .. --install-prefix /usr
+#cd /tmp/rounded-corners/build && make
+#cd /tmp/rounded-corners/build && make install
 
 # Remove development packages & Clean up
-rpm-ostree override remove cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
-rm -rf /tmp/rounded-corners
+#rpm-ostree override remove cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
+#rm -rf /tmp/rounded-corners
