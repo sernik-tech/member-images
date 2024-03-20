ef3ea4a71aa3b64464da7ed14ddb5640aac7fe13
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-20 08:54:13 -0500

    chore(wuzetka): remove kde6 repo

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -9,16 +9,6 @@ set -euo pipefail
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite.repo"
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-multilib-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo"
 
-#
-# KDE Plasma 6
-#
-wget "https://copr.fedorainfracloud.org/coprs/solopasha/kde6/repo/fedora-$(rpm -E %fedora)/solopasha-kde6-fedora-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_solopasha-kde6.repo"
-
-#                  {Bazzite packages}
-#                  |                        {KDE Plasma 6 packages}
-#                  |                        |
-rpm-ostree install joystickwake xwiimote-ng plasma-desktop plasma-workspace plasma-wayland-protocols dolphin dolphin-plugins kate breeze-gtk merkuro
-
 # disabling the repositories for the booted system
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezfurlong-wezterm-nightly-fedora-$(rpm -E %fedora).repo
@@ -26,4 +16,3 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/kylegospo-rom-properties-fedor
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/home:paul4us.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_solopasha-kde6.repo
