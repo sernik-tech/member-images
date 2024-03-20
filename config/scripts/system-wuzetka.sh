6904d73ca4867ec7dd2d9d0b195159541df7d8a3
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-20 08:43:47 -0500

    chore(wuzetka): attempt to force reinstall packages to upgrade

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -4,24 +4,26 @@
 set -euo pipefail
 
 #
-# Extra Packages
+# Bazzite
 #
-
-# adding bazzite packages
-
-# bazzite repositories
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite.repo"
-# multilib
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fedora-$(rpm -E %fedora)/kylegospo-bazzite-multilib-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo"
 
-# utilities
-rpm-ostree install joystickwake xwiimote-ng
+#
+# KDE Plasma 6
+#
+wget "https://copr.fedorainfracloud.org/coprs/solopasha/kde6/repo/fedora-$(rpm -E %fedora)/solopasha-kde6-fedora-fedora-$(rpm -E %fedora).repo" -O "/etc/yum.repos.d/_copr_solopasha-kde6.repo"
+
+#                  {Bazzite packages}
+#                  |                        {KDE Plasma 6 packages}
+#                  |                        |
+rpm-ostree install joystickwake xwiimote-ng plasma-desktop plasma-workspace plasma-wayland-protocols dolphin dolphin-plugins kate breeze-gtk merkuro
 
 # disabling the repositories for the booted system
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezfurlong-wezterm-nightly-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/kylegospo-rom-properties-fedora-$(rpm -E %fedora).repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/solopasha-kde6-fedora-fedora-$(rpm -E %fedora).repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/home:paul4us.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_solopasha-kde6.repo
