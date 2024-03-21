bcaede511eb0741ec069be3fa25f7bde68e268d2
Ruben <60184397+sneexy-boi@users.noreply.github.com>
2024-03-21 10:06:44 -0500

    chore: add more (and retest) kde6 script

--- config/scripts/kde6.sh
+++ config/scripts/kde6.sh
@@ -21,7 +21,7 @@ rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfraclo
     kf6-baloo \
     bluedevil \
     breeze-gtk \
-    breeze-icon \
+    breeze-icon-theme \
     calendarsupport \
     colord-kde \
     desktop-backgrounds\
@@ -39,9 +39,118 @@ rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfraclo
     kde-gtk-config \
     kde-inotify-survey \
     kde-partitionmanager \
+    kde-settings \
+    kdecoration \
+    kdegraphics-mobipocket \
+    kdegraphics-thumbnailers \
+    kdepim-addons \
+    kdepim-runtime \
+    kdeplasma-addons \
+    kdiagram \
+    kdialog \
+    kdnssd \
+    kdsoap \
+    keditbookmarks \
+    kf6-attica \
+    kf6-baloo \
+    kf6-bluez-qt \
+    kf6-frameworkintegration \
+    kf6-karchive \
+    kf6-kauth \
+    kf6-kbookmarks \
+    kf6-kcalendarcore \
+    kf6-kcmutils \
+    kf6-kcodecs \
+    kf6-kcompletion \
+    kf6-kconfig \
+    kf6-kconfigwidgets \
+    kf6-kcontacts \
+    kf6-kcoreaddons \
+    kf6-kcrash \
+    kf6-kdav \
+    kf6-kdbusaddons \
+    kf6-kdeclarative \
+    kf6-kded \
+    kf6-kdesu \
+    kf6-kdnssd \
+    kf6-kfilemetadata \
+    kf6-kglobalaccel \
+    kf6-kguiaddons \
+    kf6-kholidays \
+    kf6-ki18n \
+    kf6-kiconthemes \
+    kf6-kidletime \
+    kf6-kimageformats \
+    kf6-kio \
+    kf6-kirigami \
+    kf6-kirigami-addons \
+    kf6-kitemmodels \
+    kf6-kitemviews \
+    kf6-kjobwidgets \
+    kf6-knewstuff \
+    kf6-knotifications \
+    kf6-knotifyconfig \
+    kf6-kpackage \
+    kf6-kparts \
+    kf6-kpeople \
+    kf6-kpty \
+    kf6-kquickcharts \
+    kf6-krunner \
+    kf6-kservice \
+    kf6-kstatusnotifieritem \
+    kf6-ktexteditor \
+    kf6-ktexttemplate \
+    kf6-ktexteditor \
+    kf6-kunitconversion \
+    kf6-kuserfeedback \
+    kf6-kwallet \
+    kf6-kwidgetsaddons \
+    kf6-kwindowsystem \
+    kf6-kxmlgui \
+    kf6-modemmanager-qt \
+    kf6-networkmanager-qt \
+    kf6-prison \
+    kf6-purpose \
+    kf6-qqc2-desktop-style \
+    kf6-solid \
+    kf6-sonnet \
+    kf6-syndication \
+    kf6-syntax-highlighting \
+    kf6-threadweaver \
+    kfind \
+    kgpg \
+    khelpcenter \
+    kinfocenter \
+    kio-admin \
+    kio-extras \
+    kio-extras-kf5 \
+    kmenuedit \
+    konsole \
+    kpipewire \
+    kpmcore \
+    kscreen \
+    kscreenlocker \
+    ksmtp \
+    ksshaskpass \
+    ksystemlog \
+    ksystemstats \
+    ktextaddons \
+    kwalletmanager5 \
+    kwayland \
+    kwayland-integration \
+    kwin \
+    kwrited \
+    layer-shell-qt \
+    layer-shell-qt5 \
+    libgravatar \
+    libkcddb \
+    libkcompactdisc \
+    libkdcraw \
+    libkdepim \
     xdg-desktop-portal-kde \
     xdg-utils \
     xwaylandvideobridge # We attempt to update as many applications as we can that are usually found within a stock Kinoite install (although list is based on Sneexy's kinoite image, so there is likely some extras in here that aren't supposed to be here but this should be a close enough list). In alphabetical order of the repositories' list of packages.
+    # NOTE: Incomplete.
 rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:kde6' ark \
     dolphin \
     dolphin-plugins \
@@ -50,6 +159,12 @@ rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfraclo
     kalarm \
     kalk \
     kate \
-    kcm_wacomtablet # Other packages that you may or may not have installed (example, if they're in flatpaks or if they're packages you manually layer yourself)
+    kcm_wacomtablet \
+    kget \
+    kmail \
+    kmail-account-wizard \
+    kontact \
+    kontactinterface \
+    korganizer # Other packages that you may or may not have installed (example, if they're in flatpaks or if they're packages you manually layer yourself)
 # You can refer to https://copr.fedorainfracloud.org/coprs/solopasha/kde6/packages/ to see other available packages you may want to install/upgrade.
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_solopasha-kde6.repo # Disable the repository. This is optional.
