2b5007da1c065500663489de9adc0ceb0cbf47a2
Sneexy <sneexy@disroot.org>
2024-03-31 01:58:53 -0500

    chore(mazurek): add extension manager, add gpaste, add burn my windows and paperwm

--- config/scripts/system-mazurek.sh
+++ config/scripts/system-mazurek.sh
@@ -12,6 +12,8 @@ mkdir -p /usr/share/Kvantum
 mkdir -p /usr/share/qt5ct/colors
 mkdir -p /usr/share/qt6ct/colors
 mkdir -p /usr/share/icons
+mkdir -p /usr/share/gnome-shell/extensions/paperwm@paperwm.github.com
+mkdir -p /usr/share/gnome-shell/extensions/burn-my-windows@schneegans.github.com
 mkdir -p /usr/etc/skel/.local/share/themes
 mkdir -p /usr/etc/skel/.var/app/org.gnome.gedit/data/gedit/styles
 
@@ -37,6 +39,36 @@ chmod +x /tmp/papirus-folders/papirus-folders
 # Gedit
 git clone https://github.com/catppuccin/gedit.git /tmp/gedit
 cp -r /tmp/gedit/themes/* /usr/etc/skel/.var/app/org.gnome.gedit/data/gedit/styles
+
+# GNOME Extensions
+
+# PaperWM
+git clone https://github.com/paperwm/PaperWM.git /tmp/paperwm
+rm -rf /tmp/paperwm/.git
+rm -rf /tmp/paperwm/.github
+rm -rf /tmp/paperwm/.gitignore
+rm -rf /tmp/paperwm/.eslintrc.yml
+rm -rf /tmp/paperwm/media
+rm -rf /tmp/paperwm/README.md
+rm -rf /tmp/paperwm/CONTRIBUTING.md
+rm -rf /tmp/paperwm/install.sh
+rm -rf /tmp/paperwm/uninstall.sh
+cp -r /tmp/paperwm/* /usr/share/gnome-shell/extensions/paperwm@paperwm.github.com
+
+# Burn My Windows
+git clone https://github.com/Schneegans/Burn-My-Windows.git /tmp/bmw
+rm -rf /tmp/bmw/.git
+rm -rf /tmp/bmw/.github
+rm -rf /tmp/bmw/.reuse
+rm -rf /tmp/bmw/.clang-format
+rm -rf /tmp/bmw/.gitignore
+rm -rf /tmp/bmw/docs
+rm -rf /tmp/bmw/kwin
+rm -rf /tmp/bmw/scripts
+rm -rf /tmp/bmw/tests
+rm -rf /tmp/bmw/Makefile
+rm -rf /tmp/bmw/README.md
+cp -r /tmp/bmw/* /usr/share/gnome-shell/extensions/burn-my-windows@schneegans.github.com
 
 # disabling the repositories for the booted system
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-$(rpm -E %fedora).repo
