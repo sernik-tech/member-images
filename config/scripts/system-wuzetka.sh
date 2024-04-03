0fe599c496f8471ad39e344c37429345d4c2682c
Sneexy <sneexy@disroot.org>
2024-04-03 15:50:31 -0500

    chore(wuzetka): clean up the files, grab sddm theme remotely, attempt to compile kde rounded corners

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -10,54 +10,8 @@ wget "https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fe
 # bazzite packages
 rpm-ostree install joystickwake xwiimote-ng
 
-# disabling the repositories for the booted system
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-$(rpm -E %fedora).repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezfurlong-wezterm-nightly-fedora-$(rpm -E %fedora).repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/kylegospo-rom-properties-fedora-$(rpm -E %fedora).repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/home:paul4us.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
-
-# This script installs Catppuccin GTK, QT and Icon (Papirus) themes.
-# Please copy this script and make your own modifications (folders, accent colors, flavours, etc.)
-#
-# This script is recommended to be ran after installing packages.
-#
-# Required packages (for the booted/installed system):
-# papirus-icon-theme
-
-# Set your flavours and colors.
-# Refer to https://github.com/catppuccin/catppuccin#-palette
-#
-# Possible values:
-#
-# Flavours:
-#
-# latte
-# frappe
-# macchiato
-# mocha
-#
-# Accent colors:
-#
-# rosewater
-# flamingo
-# pink
-# mauve
-# red
-# maroon
-# peach
-# yellow
-# green
-# teal
-# sky
-# sapphire
-# blue
-# lavender
-FLAVOUR=mocha
-ACCENT_COLOR=green
-
 # Ensure all required/used folders are created
+mkdir -p /usr/share/sddm/themes
 mkdir -p /usr/share/themes
 mkdir -p /usr/share/icons
 mkdir -p /etc/skel/.local/share/themes
@@ -67,48 +21,50 @@ mkdir -p /etc/skel/.local/share/themes
 #
 
 # Install dependencies
-# Only required for creating the GTK theme, if you use any of these packages for your system and/or preinstall them,
-# remove installing and uninstalling the packages you use from this script
 rpm-ostree install sassc inkscape optipng
 
 git clone --recurse-submodules https://github.com/catppuccin/gtk.git /tmp/gtk # Clone the GTK theme repository
 python3 -m venv /tmp/gtk # Create python virtual environment
-source /tmp/gtk/bin/activate && pip install -r /tmp/gtk/requirements.txt # Install python dependencies within the virtual environment
-cd /tmp/gtk && source /tmp/gtk/bin/activate && python3 /tmp/gtk/install.py ${FLAVOUR} -a ${ACCENT_COLOR} -s compact -d /usr/share/themes # Install the theme system-side (May not be required for your own use-case)
-cd /tmp/gtk && source /tmp/gtk/bin/activate && python3 /tmp/gtk/install.py ${FLAVOUR} -a ${ACCENT_COLOR} -s compact -d /etc/skel/.local/share/themes # Install the theme for users (only for fresh installs and new users)
-# NOTE: You can specify `-s compact` for a more compact version of the GTK theme to be installed.
-# For example: python3 /tmp/gtk/install.py ${FLAVOUR} -a ${ACCENT_COLOR} -s compact -d /usr/share/themes
-
-#
-# KDE Theme
-#
-
-#git clone https://github.com/catppuccin/kde.git /tmp/kde # Clone the KDE theme repository
-#cd /tmp/kde && git checkout v0.2.5 # Only for KDE Plasma 5. Remove this line when we get bumped to Plasma 6
-#sed -i 's@${XDG_DATA_HOME:-$HOME/.local/share}/color-schemes@/etc/skel/.local/share/color-schemes@g' /tmp/kde/install.sh # Replace the directories in the script to the proper skeleton directory
-#sed -i 's@${XDG_DATA_HOME:-$HOME/.local/share}/aurorae/themes@/etc/skel/.local/share/aurorae/themes@g' /tmp/kde/install.sh
-#sed -i 's@${XDG_DATA_HOME:-$HOME/.local/share}/plasma/look-and-feel@/etc/skel/.local/share/plasma/look-and-feel@g' /tmp/kde/install.sh
-#sed -i 's@${XDG_DATA_HOME:-$HOME/.local/share}/icons@/etc/skel/.local/share/icons@g' /tmp/kde/install.sh
-#chmod +x /tmp/kde/install.sh # Make sure its executable
-#yes | /tmp/kde/install.sh 1 9 1
+source /tmp/gtk/bin/activate && pip install -r /tmp/gtk/requirements.txt
+cd /tmp/gtk && source /tmp/gtk/bin/activate && python3 /tmp/gtk/install.py mocha -a green -s compact -d /etc/skel/.local/share/themes
 
 #
 # Papirus (Folders)
 #
 
-# Clone the script & copy the contents to the installed icons.
-# Reminder: This means that this script has to run after installing packages.
-# Make sure you are installing the `papirus-icon-theme` package.
 git clone https://github.com/catppuccin/papirus-folders.git /tmp/papirus-folders
-cp -r /tmp/papirus-folders/src/* /usr/share/icons/Papirus # Copy the custom files to Papirus
-chmod +x /tmp/papirus-folders/papirus-folders # Making the script executable
-/tmp/papirus-folders/papirus-folders -t Papirus-Dark -C cat-${FLAVOUR}-${ACCENT_COLOR} # Change the color of the icon theme using papirus-folders
+cp -r /tmp/papirus-folders/src/* /usr/share/icons/Papirus
+chmod +x /tmp/papirus-folders/papirus-folders
+/tmp/papirus-folders/papirus-folders -t Papirus-Dark -C cat-mocha-green
+
+#
+# SDDM Theme
+#
+git clone https://github.com/aczw/sddm-theme-corners.git /tmp/corners
+sudo cp -r /tmp/corners/corners /usr/share/sddm/themes
+
+#
+# KDE Rounded Corners
+#
+rpm-ostree install git cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
+git clone https://github.com/matinlotfali/KDE-Rounded-Corners.git /tmp/roundedcorners
+mkdir /tmp/roundedcorners/build
+cd /tmp/roundedcorners/build && cmake .. --install-prefix /usr
+cd /tmp/roundedcorners/build && make && make install
 
 #
 # Clean up
 #
 
-rm -rf /tmp/gtk
-#rm -rf /tmp/kde
-rm -rf /tmp/papirus-folders
-rpm-ostree override remove sassc inkscape optipng
+rm -rf /tmp/*
+
+# remove deps installed from building packages and stuff from source
+rpm-ostree override remove GraphicsMagick GraphicsMagick-c++ boost-atomic boost-filesystem flexiblas flexiblas-netlib flexiblas-openblas-openmp gsl gtksourceview4 inkscape inkscape-libs lib2geom libcdr libgfortran libquadmath librevenge libsass libvisio libwpd libwpg mkfontscale openblas openblas-openmp optipng potrace python3-cssselect python3-inkex python3-lxml python3-numpy python3-pyparsing python3-pyserial python3-scour python3-six sassc urw-base35-fonts-legacy
+
+# disabling the repositories for the booted system
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/atim-starship-fedora-$(rpm -E %fedora).repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezfurlong-wezterm-nightly-fedora-$(rpm -E %fedora).repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/kylegospo-rom-properties-fedora-$(rpm -E %fedora).repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/home:paul4us.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo
