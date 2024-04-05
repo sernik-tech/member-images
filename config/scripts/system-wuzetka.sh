3beb55969dce4a97f9438d9d177f3a1aaeb80f36
Sneexy <sneexy@disroot.org>
2024-04-05 11:45:37 -0500

    chore(wuzetka): modify more flatpaks

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -13,6 +13,7 @@ rpm-ostree install joystickwake xwiimote-ng
 # Ensure all required/used folders are created
 mkdir -p /usr/share/sddm/themes
 mkdir -p /usr/share/kwin/effects
+mkdir -p /usr/share/plasma/plasmoids/org.kde.latte.spacer
 mkdir -p /usr/share/themes
 mkdir -p /usr/share/icons
 mkdir -p /usr/etc/skel/.local/share/themes
@@ -64,6 +65,17 @@ git clone https://github.com/aczw/sddm-theme-corners.git /tmp/corners
 sudo cp -r /tmp/corners/corners /usr/share/sddm/themes
 
 #
+# Latte spacer
+#
+git clone https://github.com/psifidotos/applet-latte-spacer.git /tmp/latte-spacer
+# plasma 6 git
+#git clone https://github.com/doncsugar/applet-latte-spacer.git /tmp/latte-spacer
+rm -rf /tmp/latte-spacer/.git
+rm -f /tmp/latte-spacer/README.md
+rm -f /tmp/latte-spacer/CHANGELOG.md
+cp -r /tmp/latte-spacer/* /usr/share/plasma/plasmoids/org.kde.latte.spacer
+
+#
 # KDE Rounded Corners
 #
 rpm-ostree install git cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
@@ -80,6 +92,15 @@ git clone https://github.com/Schneegans/Burn-My-Windows.git /tmp/bmw
 chmod +x /tmp/bmw/kwin/build.sh
 cd /tmp/bmw && /tmp/bmw/kwin/build.sh
 tar -xf /tmp/bmw/kwin/burn_my_windows_kwin4.tar.gz -C /usr/share/kwin/effects
+
+#
+# Panel Colorizer
+#
+#rpm-ostree install libplasma plasma5support
+#git clone git clone https://github.com/luisbocanegra/plasma-panel-colorizer /tmp/panel-colorizer
+#cd /tmp/panel-colorizer && cmake -B build -S . -DCMAKE_INSTALL_PREFIX=/usr
+#cd /tmp/panel-colorizer && cmake --build build
+#cd /tmp/panel-colorizer && cmake --install build
 
 #
 # Clean up
