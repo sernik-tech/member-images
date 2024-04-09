3b7fc4f20f330a76f173d17711c999b40c128a38
Sneexy <sneexy@disroot.org>
2024-04-08 21:12:19 -0500

    chore(rounded-corners-kwin): accidentally installed a bunch of deps that shouldn't be needed

---
+++ config/scripts/kde-material-you-plasma5.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+# Install python3-pip if it's not already installed
+if ! rpm -q python3-pip > /dev/null; then
+    rpm-ostree install python3-pip
+fi
+
+# Install the backend and other dependicies with pip
+pip install --prefix=/usr kde-material-you-colors
+pip install --prefix=/usr pywal
--- config/scripts/rounded-corners-kwin.sh
+++ config/scripts/rounded-corners-kwin.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # Install required build dependencies
-rpm-ostree install cmake cmake-data extra-cmake-modules git jsoncpp kf5-kauth-devel kf5-kcodecs-devel kf5-kconfig kf5-kconfig-devel kf5-kconfigwidgets-devel kf5-kcoreaddons-devel kf5-kservice-devel kf5-kwidgetsaddons-devel kf5-kwindowsystem-devel kf5-rpm-macros kwin-devel libX11-devel libXau-devel libepoxy-devel libglvnd-core-devel libglvnd-devel libxcb-devel mesa-libEGL-devel perl-English perl-Error perl-File-Find perl-Git perl-TermReadKey qt5-linguist qt5-qtbase-devel qt5-rpm-macros rhash vulkan-headers vulkan-loader-devel xorg-x11-proto-devel
+rpm-ostree install git cmake gcc-c++ extra-cmake-modules kwin-devel kf5-kconfigwidgets-devel libepoxy-devel
 
 # Create kwin effect's folder
 mkdir -p /usr/share/kwin/effects
@@ -16,4 +16,4 @@ cd /tmp/roundedcorners/build && cmake .. --install-prefix /usr
 cd /tmp/roundedcorners/build && make && make install
 
 # Remove build dependencies
-rpm-ostree override remove cmake cmake-data extra-cmake-modules git jsoncpp kf5-kauth-devel kf5-kcodecs-devel kf5-kconfig kf5-kconfig-devel kf5-kconfigwidgets-devel kf5-kcoreaddons-devel kf5-kservice-devel kf5-kwidgetsaddons-devel kf5-kwindowsystem-devel kf5-rpm-macros kwin-devel libX11-devel libXau-devel libepoxy-devel libglvnd-core-devel libglvnd-devel libxcb-devel mesa-libEGL-devel perl-English perl-Error perl-File-Find perl-Git perl-TermReadKey qt5-linguist qt5-qtbase-devel qt5-rpm-macros rhash vulkan-headers vulkan-loader-devel xorg-x11-proto-devel
+rpm-ostree override remove cmake cmake-data extra-cmake-modules jsoncpp kf5-kauth-devel kf5-kcodecs-devel kf5-kconfig kf5-kconfig-devel kf5-kconfigwidgets-devel kf5-kcoreaddons-devel kf5-kservice-devel kf5-kwidgetsaddons-devel kf5-kwindowsystem-devel kf5-rpm-macros kwin-devel libX11-devel libXau-devel libepoxy-devel libglvnd-core-devel libglvnd-devel libxcb-devel mesa-libEGL-devel perl-English perl-Error perl-File-Find perl-Git perl-TermReadKey qt5-linguist qt5-qtbase-devel qt5-rpm-macros rhash vulkan-headers vulkan-loader-devel xorg-x11-proto-devel
