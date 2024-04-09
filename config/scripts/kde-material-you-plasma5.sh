c2f59ead7880dde223ba5bfbd95b4f97632307d6
Sneexy <sneexy@disroot.org>
2024-04-08 21:52:21 -0500

    feat: add kde-material-you-plasma5.sh

--- config/scripts/kde-material-you-plasma5.sh
+++ config/scripts/kde-material-you-plasma5.sh
@@ -12,4 +12,14 @@ fi
 pip install --prefix=/usr kde-material-you-colors
 pip install --prefix=/usr pywal
 
-rpm-ostree install gcc
+# First 3 deps are related to the backend, the rest is related to the plasma widget
+rpm-ostree install gcc dbus-python-devel glib2-devel extra-cmake-modules qt5-qttools-devel kf5-plasma-devel
+
+git clone https://github.com/luisbocanegra/kde-material-you-colors /tmp/kde-material-you
+cd /tmp/kde-material-you && git checkout plasma5
+cd /tmp/kde-material-you && cmake -B build -S . -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_PLASMOID=ON
+cd /tmp/kde-material-you && cmake --build build
+cd /tmp/kde-material-you && cmake --install build
+
+# Remove
+rpm-ostree install gcc dbus-python-devel glib2-devel extra-cmake-modules qt5-qttools-devel kf5-plasma-devel
