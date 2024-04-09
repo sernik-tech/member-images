af081f53cf33c3d8d497786df310690ad8a616e8
Sneexy <sneexy@disroot.org>
2024-04-09 08:42:15 -0500

    fix(kde-material-you): somehow didn't change the package name for python3-dbus

--- config/scripts/kde-material-you-plasma5.sh
+++ config/scripts/kde-material-you-plasma5.sh
@@ -9,7 +9,7 @@ if ! rpm -q python3-pip > /dev/null; then
 fi
 
 # First set of packages are related to the backend, second set for pillow, the rest is related to the plasma widget
-rpm-ostree install gcc dbus-python dbus-python-devel glib2 glib2-devel \
+rpm-ostree install gcc python3-dbus dbus-python-devel glib2 glib2-devel \
     zlib zlib-devel libjpeg-turbo libjpeg-turbo-devel libtiff libtiff-devel lcms2 lcms2-devel libwebp libwebp-devel openjpeg2 openjpeg2-devel libimagequant libimagequant-devel libraqm libraqm-devel libxcb \
     extra-cmake-modules qt5-qttools-devel kf5-plasma-devel
 
