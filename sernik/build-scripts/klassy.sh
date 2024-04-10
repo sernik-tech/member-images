3acdcb8e04012016e01a7c3c22c310766e7e394e
Sneexy <sneexy@disroot.org>
2024-04-10 18:55:28 -0500

    chore(containers): actually ass klassy

--- sernik/build-scripts/klassy.sh
+++ sernik/build-scripts/klassy.sh
@@ -4,15 +4,15 @@
 set -euo pipefail
 
 # Install required build dependencies
-dnf install -y git cmake gcc-c++ extra-cmake-modules kwin-devel kf6-kconfigwidgets-devel libepoxy-devel kf6-kcmutils-devel qt6-qtbase-private-devel wayland-devel
+dnf install -y git cmake extra-cmake-modules gettext "cmake(KF5Config)" "cmake(KF5CoreAddons)" "cmake(KF5FrameworkIntegration)"  "cmake(KF5GuiAddons)" "cmake(KF5Kirigami2)" "cmake(KF5WindowSystem)" "cmake(KF5I18n)" "cmake(Qt5DBus)" "cmake(Qt5Quick)" "cmake(Qt5Widgets)" "cmake(Qt5X11Extras)" "cmake(KDecoration2)" "cmake(KF6ColorScheme)" "cmake(KF6Config)" "cmake(KF6CoreAddons)" "cmake(KF6FrameworkIntegration)" "cmake(KF6GuiAddons)" "cmake(KF6I18n)" "cmake(KF6KCMUtils)" "cmake(KF6KirigamiPlatform)" "cmake(KF6WindowSystem)" "cmake(Qt6Core)" "cmake(Qt6DBus)" "cmake(Qt6Quick)" "cmake(Qt6Svg)" "cmake(Qt6Widgets)" "cmake(Qt6Xml)"
 
 # Clone, build and install the effects
 cd /tmp
-git clone --single-branch --depth=1 https://github.com/matinlotfali/KDE-Rounded-Corners.git
-cd KDE-Rounded-Corners
+git clone --single-branch --depth=1 https://github.com/paulmcauley/klassy.git
+cd klassy
+git checkout 6.0.breeze6.0.2
 mkdir build
 cd build
-cmake .. --install-prefix /usr
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 make
-make install DESTDIR=/tmp/rounded-corners-built PREFIX='/usr'
-ls /tmp/rounded-corners-built
+make install DESTDIR=/tmp/klassy-built PREFIX='/usr'
