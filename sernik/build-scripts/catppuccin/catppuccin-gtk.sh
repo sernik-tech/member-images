e7389772ea4ef4b9b786a09820660d322b827473
Sneexy <sneexy@disroot.org>
2024-04-11 09:19:49 -0500

    chore(containers): i keep forgetting its dnf and not rpm-ostree

--- sernik/build-scripts/catppuccin/catppuccin-gtk.sh
+++ sernik/build-scripts/catppuccin/catppuccin-gtk.sh
@@ -10,7 +10,7 @@ GTKDIR=/tmp/catppuccin-gtk
 mkdir -p $GTKDIR
 
 # Dependencies to create theme
-rpm-ostree install sassc inkscape optipng
+dnf install -y sassc inkscape optipng
 cd /tmp
 # Clone repository into /tmp/gtk
 git clone --recurse-submodules https://github.com/catppuccin/gtk.git
@@ -18,7 +18,7 @@ cd gtk
 # Python setup
 python3 -m venv venv # Create python virtual environment
 source venv/bin/activate
-pip install -r /tmp/gtk/requirements.txt
+pip install -r requirements.txt
 # Create theme and install to user directory
-python3 /tmp/gtk/install.py all -a all -d $GTKDIR
-python3 /tmp/gtk/install.py all -a all -s compact -d $GTKDIR
+python3 install.py all -a all -d $GTKDIR
+python3 install.py all -a all -s compact -d $GTKDIR
