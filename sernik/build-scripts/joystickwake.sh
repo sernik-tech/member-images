938fa96a318b03f6e8f92884a840c8fbbd6ff9f0
Sneexy <sneexy@disroot.org>
2024-04-10 21:47:56 -0500

    chore(containers): don't use joystickwake's setuo.py

--- sernik/build-scripts/joystickwake.sh
+++ sernik/build-scripts/joystickwake.sh
@@ -8,12 +8,17 @@ dnf install -y git gzip python3 python3-docutils
 
 # dirs
 mkdir -p /tmp/joystickwake-built/usr/share/man/man1
+mkdir -p /tmp/joystickwake-built/usr/bin
+mkdir -p /tmp/joystickwake-built/usr/etc/xdg/autostart
 
 # Clone, build and install the effects
 cd /tmp
 git clone --single-branch --depth=1 https://github.com/foresto/joystickwake.git
 cd joystickwake
-python3 setup.py install --prefix=/usr --root=/tmp/joystickwake-built
+chmod +x joystickwake
+chmod +x joystickwake.desktop
+mv joystickwake /tmp/joystickwake-built/usr/bin
+mv joystickwake.desktop /tmp/joystickwake-built/usr/etc/xdg/autostart
 rst2man README.rst joystickwake.1
 gzip joystickwake.1
 mv joystickwake.1.gz /tmp/joystickwake-built/usr/share/man/man1
