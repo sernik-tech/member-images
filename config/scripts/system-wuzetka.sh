66f19bb123bd5f8f5b5f94cba60a7c26b1efbcc0
Sneexy <sneexy@disroot.org>
2024-04-13 14:33:19 -0500

    feat(wuzetka): add justfiles from bazzite

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -77,3 +77,10 @@ cd /tmp/papirus-colors && /tmp/papirus-colors/places_icons_mod.sh
 #
 git clone https://git.gay/sneexy/sernik.git /tmp/wuzetka
 cp -r /tmp/wuzetka/kde/wuzetka* /usr/share/plasma/look-and-feel
+
+#
+# Justfiles
+#
+wget "https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/share/ublue-os/just/82-bazzite-waydroid.just" -O "/usr/share/ublue-os/just/31-waydroid.just"
+wget "https://github.com/ublue-os/bazzite/blob/main/system_files/desktop/shared/usr/share/ublue-os/just/81-bazzite-fixes.just" -O "/usr/share/ublue-os/just/62-bazzite-fixes.just"
+wget "https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/share/ublue-os/just/83-bazzite-audio.just" -O "/usr/share/ublue-os/just/41-audio.just"
