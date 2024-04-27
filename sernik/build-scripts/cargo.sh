f5698524cfdc31154192477f352da47751ef1333
Sneexy <sneexy@disroot.org>
2024-04-27 18:05:01 -0500

    chore:add gcc-c++ and cmake to cargo build scripts

--- sernik/build-scripts/cargo.sh
+++ sernik/build-scripts/cargo.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # Dependency and folder
-dnf install -y git gcc openssl openssl-devel cargo
+dnf install -y git gcc gcc-c++ cmake openssl openssl-devel cargo
 mkdir -p /tmp/cargo-built/usr/bin
 
 # Atuin - https://github.com/atuinsh/atuin
