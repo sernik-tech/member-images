fd293e488b092eacfd26936ac47c14d168486aff
Sneexy <sneexy@disroot.org>
2024-05-26 17:41:31 -0500

    chore(wuzetka): fix floorp icons

--- config/scripts/system-wuzetka.sh
+++ config/scripts/system-wuzetka.sh
@@ -13,6 +13,11 @@ mkdir -p /usr/share/sddm/themes
 # Floorp fixes
 #
 sed -i 's@/opt/floorp/floorp@/usr/lib/opt/floorp/floorp@g' /usr/bin/floorp
+ln -sf /lib/opt/floorp/browser/chrome/icons/default/default128.png /usr/share/icons/hicolor/128x128/apps/floorp.png
+ln -sf /lib/opt/floorp/browser/chrome/icons/default/default64.png /usr/share/icons/hicolor/64x64/apps/floorp.png
+ln -sf /lib/opt/floorp/browser/chrome/icons/default/default48.png /usr/share/icons/hicolor/48x48/apps/floorp.png
+ln -sf /lib/opt/floorp/browser/chrome/icons/default/default32.png /usr/share/icons/hicolor/32x32/apps/floorp.png
+ln -sf /lib/opt/floorp/browser/chrome/icons/default/default16.png /usr/share/icons/hicolor/16x16/apps/floorp.png
 
 #
 # Blender fixes
