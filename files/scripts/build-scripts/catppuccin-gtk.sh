c54b2e81d53d2657f4bc224016a30a1d945f3cf3
Sneexy <sneexy@disroot.org>
2024-08-05 13:49:38 -0500

    chore(catpuccin-gtk): use colloid instead of catppuccin/gtk

--- files/scripts/build-scripts/catppuccin-gtk.sh
+++ files/scripts/build-scripts/catppuccin-gtk.sh
@@ -3,21 +3,22 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
+# Originally, this script used the official catppuccin gtk repo, but that has since been archived.
+# However, the theme it was based on, Colloid, includes catppuccin officially, so we just use that now.
+
 # Create theme directory
 mkdir -p /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes
 
 # Dependencies to create theme
-dnf install -y git python3 python3-pip sassc inkscape optipng
+dnf install -y git sassc inkscape optipng gnome-themes-extra gtk-murrine-engine
 cd /tmp
 # Clone repository into /tmp/gtk
-git clone --recurse-submodules https://github.com/catppuccin/gtk.git
-cd gtk
-# Python setup
-python3 -m venv venv # Create python virtual environment
-source venv/bin/activate
-pip install -r requirements.txt
-# Create theme and install
-python3 build.py latte --dest /tmp/catppuccin-gtk/usr/share/themes -a green
-python3 build.py mocha --dest /tmp/catppuccin-gtk/usr/share/themes -a green
-python3 build.py latte --dest /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes -a green
-python3 build.py mocha --dest /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes -a green
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
+# Install theme
+# NOTE: Specifying "catpuccin black" is mocha.
+chmod +x install.sh
+./install.sh -d /tmp/catppuccin-gtk/usr/share/themes -c dark -t green -s compact --tweaks catppuccin
+./install.sh -d /tmp/catppuccin-gtk/usr/share/themes -c light -t green -s compact --tweaks catppuccin black
+./install.sh -d /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes -c dark -t green -s compact --tweaks catppuccin
+./install.sh -d /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes -c light -t green -s compact --tweaks catppuccin black
