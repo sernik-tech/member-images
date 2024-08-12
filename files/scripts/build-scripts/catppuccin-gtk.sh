78a169d8357b8d2a45e269fe8cc3d7d69cd18777
Sneexy <sneexy@disroot.org>
2024-08-11 20:46:01 -0500

    chore(wuzetka): migrate /usr/etc to /etc

--- files/scripts/build-scripts/catppuccin-gtk.sh
+++ files/scripts/build-scripts/catppuccin-gtk.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # However, the theme it was based on, Colloid, includes catppuccin officially, so we just use that now.
 
 # Create theme directory
-mkdir -p /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes
+mkdir -p /tmp/catppuccin-gtk/etc/skel/.local/share/themes
 
 # Dependencies to create theme
 dnf install -y git sassc inkscape optipng gnome-themes-extra gtk-murrine-engine
@@ -20,5 +20,5 @@ cd Colloid-gtk-theme
 chmod +x install.sh
 ./install.sh -d /tmp/catppuccin-gtk/usr/share/themes -c dark -t green -s compact --tweaks catppuccin black
 ./install.sh -d /tmp/catppuccin-gtk/usr/share/themes -c light -t green -s compact --tweaks catppuccin
-./install.sh -d /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes -c dark -t green -s compact --tweaks catppuccin black
-./install.sh -d /tmp/catppuccin-gtk/usr/etc/skel/.local/share/themes -c light -t green -s compact --tweaks catppuccin
+./install.sh -d /tmp/catppuccin-gtk/etc/skel/.local/share/themes -c dark -t green -s compact --tweaks catppuccin black
+./install.sh -d /tmp/catppuccin-gtk/etc/skel/.local/share/themes -c light -t green -s compact --tweaks catppuccin
--- files/scripts/build-scripts/joystickwake.sh
+++ files/scripts/build-scripts/joystickwake.sh
@@ -9,7 +9,7 @@ dnf install -y git gzip python3 python3-docutils
 # dirs
 mkdir -p /tmp/joystickwake-built/usr/share/man/man1
 mkdir -p /tmp/joystickwake-built/usr/bin
-mkdir -p /tmp/joystickwake-built/usr/etc/xdg/autostart
+mkdir -p /tmp/joystickwake-built/etc/xdg/autostart
 
 # Clone, build and install the effects
 cd /tmp
@@ -18,7 +18,7 @@ cd joystickwake
 chmod +x joystickwake
 chmod +x joystickwake.desktop
 mv joystickwake /tmp/joystickwake-built/usr/bin
-mv joystickwake.desktop /tmp/joystickwake-built/usr/etc/xdg/autostart
+mv joystickwake.desktop /tmp/joystickwake-built/etc/xdg/autostart
 rst2man README.rst joystickwake.1
 gzip joystickwake.1
 mv joystickwake.1.gz /tmp/joystickwake-built/usr/share/man/man1
