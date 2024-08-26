e1704584239f1552e88305ca228feffb32a13063
Sneexy <sneexy@disroot.org>
2024-08-25 19:35:18 -0500

    chore(wuzetka): its zen-browser not zen

--- files/scripts/system-wuzetka.sh
+++ files/scripts/system-wuzetka.sh
@@ -25,7 +25,7 @@ ln -sf /lib/opt/floorp/browser/chrome/icons/default/default16.png /usr/share/ico
 #
 # Zen Browser fixes
 #
-sed -i 's@/opt/zen/zen@/usr/lib/opt/zen/zen@g' /usr/bin/zen
+sed -i 's@/opt/zen/zen@/usr/lib/opt/zen/zen@g' /usr/bin/zen-browser
 ln -sf /lib/opt/zen/browser/chrome/icons/default/default128.png /usr/share/icons/hicolor/128x128/apps/zen-browser.png
 ln -sf /lib/opt/zen/browser/chrome/icons/default/default64.png /usr/share/icons/hicolor/64x64/apps/zen-browser.png
 ln -sf /lib/opt/zen/browser/chrome/icons/default/default48.png /usr/share/icons/hicolor/48x48/apps/zen-browser.png
