795d4fa75cc8e45c09e33bebdcf8966c82dc2bc0
Sneexy <sneexy@disroot.org>
2024-09-03 14:03:45 -0500

    chore(mazurek-helper, mazurek): more deps, more deps to image

--- files/scripts/build-scripts/wayland.sh
+++ files/scripts/build-scripts/wayland.sh
@@ -6,7 +6,9 @@ set -euo pipefail
 # Dependency and folder
 dnf install -y git gcc gcc-c++ cmake openssl protobuf protobuf-devel protobuf-compiler openssl-devel dbus-devel pkgconf-pkg-config \
     rustup cargo meson ninja-build \
-    wayland-protocols-devel rust-libpulse-binding-devel rust-wayland-client-devel rust-wayland-protocols+wayland-client-devel egl-wayland-devel egl-wayland egl-utils mesa-libEGL mesa-libEGL-devel
+    wayland-protocols-devel rust-libpulse-binding-devel rust-wayland-client-devel rust-wayland-protocols+wayland-client-devel \
+    egl-wayland-devel egl-wayland egl-utils mesa-libEGL mesa-libEGL-devel \
+    sassc rust-gdk0.17+v3_24-devel rust-libudev-devel gtk-layer-shell-devel libevdev-devel
 mkdir -p /tmp/wayland-built/usr/bin
 
 # SwayAudioIdleInhibit - https://github.com/ErikReider/SwayAudioIdleInhibit
@@ -26,3 +28,11 @@ cargo build --release
 cd target/release
 cp wpaperd /tmp/wayland-built/usr/bin
 cp wpaperctl /tmp/wayland-built/usr/bin
+
+# SwayOSD - https://github.com/ErikReider/SwayOSD
+cd /tmp
+git clone https://github.com/ErikReider/SwayOSD
+cd SwayOSD
+meson setup build
+ninja -C build
+meson install -C build
