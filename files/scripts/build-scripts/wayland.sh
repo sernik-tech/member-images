8f0b459b1f0c108ed2654a6b63cded978eb439e6
Sneexy <sneexy@disroot.org>
2024-09-03 15:05:27 -0500

    chore(mazurek-helper): proper locations, use rinstall

--- files/scripts/build-scripts/wayland.sh
+++ files/scripts/build-scripts/wayland.sh
@@ -7,9 +7,17 @@ set -euo pipefail
 dnf install -y git gcc gcc-c++ cmake openssl protobuf protobuf-devel protobuf-compiler openssl-devel dbus-devel pkgconf-pkg-config \
     rustup cargo meson ninja-build \
     wayland-protocols-devel rust-libpulse-binding-devel rust-wayland-client-devel rust-wayland-protocols+wayland-client-devel \
-    egl-wayland-devel egl-wayland egl-utils mesa-libEGL mesa-libEGL-devel \
+    scdoc egl-wayland-devel egl-wayland egl-utils mesa-libEGL mesa-libEGL-devel \
     sassc rust-gdk0.17+v3_24-devel rust-libudev-devel gtk-layer-shell-devel libevdev-devel libinput-devel
 mkdir -p /tmp/wayland-built/usr/bin
+
+# rinstall (Only used here for some packages)
+cd /tmp
+git clone https://github.com/DanySpin97/rinstall
+cd rinstall
+cargo build --release
+chmod +x ./target/release/rinstall
+./target/release/rinstall install -y    
 
 # SwayAudioIdleInhibit - https://github.com/ErikReider/SwayAudioIdleInhibit
 cd /tmp
@@ -17,14 +25,16 @@ git clone https://github.com/ErikReider/SwayAudioIdleInhibit
 cd SwayAudioIdleInhibit
 meson build
 ninja -C build
-meson install -C build --destdir "/tmp/wayland-built/usr"
-#cp build/sway-audio-idle-inhibit /tmp/wayland-built/usr/bin
+meson install -C build --destdir "/tmp/wayland-built"
 
 # wpaperd - https://github.com/danyspin97/wpaperd
 cd /tmp
 git clone https://github.com/danyspin97/wpaperd
 cd wpaperd
 cargo build --release
+scdoc < man/wpaperd-output.5.scd > man/wpaperd-output.5
+rinstall --destdir "/tmp/wayland-built" --yes
+
 cd target/release
 cp wpaperd /tmp/wayland-built/usr/bin
 cp wpaperctl /tmp/wayland-built/usr/bin
@@ -35,4 +45,4 @@ git clone https://github.com/ErikReider/SwayOSD
 cd SwayOSD
 meson setup build
 ninja -C build
-meson install -C build --destdir "/tmp/wayland-built/usr"
+meson install -C build --destdir "/tmp/wayland-built"
